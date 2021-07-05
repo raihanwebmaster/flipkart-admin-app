5e69f97538fd413f98d0dabfdcf976ac5f95a1b9
raihanwebmaster <raihanemon2015@gmail.com>
2021-07-05 13:18:43 +0200

    fix-categories-delete-and-ui-update

---
+++ src/containers/Category/components/UpdateCategoiresModal.js
@@ -0,0 +1,128 @@
+import Input from "../../../components/UI/Input/Input";
+import NewModal from "../../../components/UI/Modal/modal";
+import { Col, Row } from "react-bootstrap";
+const UpdateCategoriesModal = (props) => {
+  const {
+    size,
+    handleClose,
+    modalTitle,
+    expandedArray,
+    checkedArray,
+    handleCategoryInput,
+    categoryList,
+    show,
+  } = props;
+  return (
+    <NewModal
+      show={show}
+      handleClose={handleClose}
+      modalTitle={modalTitle}
+      size={size}
+    >
+      <Row>
+        <Col>
+          <h6>Expanded</h6>
+        </Col>
+      </Row>
+      {expandedArray.length > 0 &&
+        expandedArray.map((item, index) => (
+          <Row key={index}>
+            <Col>
+              <Input
+                value={item.name}
+                placeholder={`Category Name`}
+                onChange={(e) =>
+                  handleCategoryInput("name", e.target.value, index, "expanded")
+                }
+              />
+            </Col>
+            <Col>
+              <select
+                className="form-control"
+                value={item.parentId}
+                onChange={(e) =>
+                  handleCategoryInput(
+                    "parentId",
+                    e.target.value,
+                    index,
+                    "expanded"
+                  )
+                }
+              >
+                <option>select category</option>
+                {categoryList.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.name}
+                  </option>
+                ))}
+              </select>
+            </Col>
+            <Col>
+              <select className="form-control">
+                <option value="">Select Type</option>
+                <option value="store">Store</option>
+                <option value="product">Product</option>
+                <option value="page">Page</option>
+              </select>
+            </Col>
+          </Row>
+        ))}
+      <Row>
+        <Col>
+          <h6>Checked</h6>
+        </Col>
+      </Row>
+      {checkedArray.length > 0 &&
+        checkedArray.map((item, index) => (
+          <Row key={index}>
+            <Col>
+              <Input
+                value={item.name}
+                placeholder={`Category Name`}
+                onChange={(e) =>
+                  handleCategoryInput("name", e.target.value, index, "checked")
+                }
+              />
+            </Col>
+            <Col>
+              <select
+                className="form-control"
+                value={item.parentId}
+                onChange={(e) =>
+                  handleCategoryInput(
+                    "parentId",
+                    e.target.value,
+                    index,
+                    "checked"
+                  )
+                }
+              >
+                <option>select category</option>
+                {categoryList.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.name}
+                  </option>
+                ))}
+              </select>
+            </Col>
+            <Col>
+              <select className="form-control">
+                <option value="">Select Type</option>
+                <option value="store">Store</option>
+                <option value="product">Product</option>
+                <option value="page">Page</option>
+              </select>
+            </Col>
+          </Row>
+        ))}
+
+      {/* <input
+          type="file"
+          name="categoryImage"
+          onChange={handleCategoryImage}
+        /> */}
+    </NewModal>
+  );
+};
+
+export default UpdateCategoriesModal;
